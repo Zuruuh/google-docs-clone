2055a50c52ce2bdeea1b8467e940f64bb3a323c0
Younes Ziadi <ziadi.mail.pro@gmail.com>
2021-07-18 07:06:39 +0200

    Added PropTypes verification

--- components/DocumentTemplate.js
+++ components/DocumentTemplate.js
@@ -1,10 +1,10 @@
-import Image from "next/Image";
+import ContextMenu from "./ContextMenu";
+import Image from "next/image";
 
+import PropTypes from "prop-types";
 import { useState } from "react";
 
-import ContextMenu from "../components/ContextMenu";
-
-function DocumentTemplate(props) {
+function DocumentTemplate({ source, text }) {
   function capitalize(s) {
     return s[0].toUpperCase() + s.slice(1);
   }
@@ -15,22 +15,27 @@ function DocumentTemplate(props) {
       <div className="mx-2">
         <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700 ">
           <Image
-            src={props.source}
+            src={source}
             layout="fill"
             onClick={() => setShowModal(true)}
           />
         </div>
         <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">
-          {capitalize(props.text)}
+          {capitalize(text)}
         </p>
       </div>
       <ContextMenu
         type="newDoc"
-        template={props.text}
+        template={text}
         states={[showModal, setShowModal, input, setInput]}
       />
     </>
   );
 }
+
+DocumentTemplate.propTypes = {
+  source: PropTypes.object.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default DocumentTemplate;
