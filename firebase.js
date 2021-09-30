a2e24f2c40fc49c85e8f42788281b3b5e4d63ad1
Zuruh <77583853+Zuruuh@users.noreply.github.com>
2021-09-30 09:26:57 +0200

    feat: added firebase config

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  apiKey: "AIzaSyA7-2XExDwu_2xF0pK0YKYhfMDWUYW9s20",
+  authDomain: "zuruh--docs-clone.firebaseapp.com",
+  projectId: "zuruh--docs-clone",
+  storageBucket: "zuruh--docs-clone.appspot.com",
+  messagingSenderId: "501146583380",
+  appId: "1:501146583380:web:a8cd5f5895fe90581f48c1",
 };
 
 const app = !firebase.apps.length
